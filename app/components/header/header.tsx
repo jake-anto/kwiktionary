29599d5fc4ea33168f60359c07e43fda4979bfe6
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-03-27 11:09:35 +0000

    Make Header sticky

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,10 +1,20 @@
 import SearchBar from "@/app/components/header/search";
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
 export default function Header() {
   return (
-    <Container maxWidth="xs" sx={{ p: 2 }}>
-      <SearchBar />
-    </Container>
+    <Box
+      component="header"
+      sx={{
+        position: "sticky",
+        top: 0,
+        zIndex: 1000,
+      }}
+    >
+      <Container maxWidth="xs" sx={{ p: 2 }}>
+        <SearchBar />
+      </Container>
+    </Box>
   );
 }
