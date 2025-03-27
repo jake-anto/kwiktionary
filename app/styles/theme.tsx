3b0671a70fa0a27c76d3adfa71a45fa6d76f68f4
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-03-27 11:25:19 +0000

    Improve routing for links in gloss

--- app/styles/theme.tsx
+++ app/styles/theme.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { createTheme } from "@mui/material/styles";
+import Link from "next/link";
 
 const theme = createTheme({
   typography: {
     fontFamily: "var(--font-roboto)",
+  },
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: Link,
+      },
+    },
   },
 });
 
