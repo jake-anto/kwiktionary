ab65725ff2a23cd0246b840d84f80bcb1d3a853c
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-04 12:31:59 +0000

    Add footer with licensing and attribution information

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,59 @@
+import { Box, Typography, Link, Button } from "@mui/material";
+import { Github } from "lucide-react";
+import { Logo } from "../logo";
+
+const LINK_PROPS = {
+  target: "_blank",
+  rel: "noopener noreferrer",
+};
+
+export default function Footer() {
+  return (
+    <Box component="footer" sx={{ p: 2, textAlign: "center", mt: 2 }}>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          Definitions, pronunciations, and other linguistic data are provided by{" "}
+          <Link href="https://www.wiktionary.org/" {...LINK_PROPS}>
+            Wiktionary
+          </Link>{" "}
+          contributors.
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          This content is available under{" "}
+          <Link
+            href="https://creativecommons.org/licenses/by-sa/4.0/"
+            {...LINK_PROPS}
+          >
+            CC BY-SA 4.0
+          </Link>{" "}
+          and{" "}
+          <Link href="https://www.gnu.org/copyleft/fdl.html" {...LINK_PROPS}>
+            GFDL
+          </Link>
+          .
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          <Logo props={{ component: "span" }} /> is an independent open-source
+          project licensed under the{" "}
+          <Link
+            href="https://github.com/jake-anto/kwiktionary/blob/dev/LICENSE"
+            {...LINK_PROPS}
+          >
+            MIT License
+          </Link>
+          .
+        </Typography>{" "}
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          Kwiktionary is not affiliated with, sponsored, or endorsed by the
+          Wikimedia Foundation, the operator of Wiktionary.
+        </Typography>
+      </Box>
+      <Button startIcon={<Github />}>Source Code</Button>
+      <Typography sx={{ color: "text.secondary" }}>
+        Made with ❤️ by Jake.
+      </Typography>
+    </Box>
+  );
+}
