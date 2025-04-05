c0744ee1796eec4d08654584ed8d238d30da4f8c
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-05 14:42:15 +0530

    Create a service worker with Serwist

---
+++ app/service worker/sw.ts
@@ -0,0 +1,50 @@
+import { defaultCache } from "@serwist/next/worker";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import {
+  CacheableResponsePlugin,
+  ExpirationPlugin,
+  RegExpRoute,
+  Serwist,
+  StaleWhileRevalidate,
+} from "serwist";
+
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
+
+declare const self: ServiceWorkerGlobalScope;
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
+
+// Cache all assets for /en/* route
+serwist.registerRoute(
+  new RegExpRoute(
+    /^\/en\/.*/,
+    new StaleWhileRevalidate({
+      cacheName: "kwiktionary-en-shell",
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200] }),
+        new ExpirationPlugin({
+          maxEntries: 1,
+          maxAgeSeconds: 60 * 60 * 24, // 1 day
+        }),
+        {
+          cacheKeyWillBeUsed: async ({}) => {
+            return "en/_app_shell";
+          },
+        },
+      ],
+    }),
+    "GET"
+  )
+);
+
+// TODO: cache API responses
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/service worker/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV !== "production",
+});
 
 const nextConfig: NextConfig = {
   // output: "export",
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
