2479304db48f0f1b5c7368345cfd3462b3c68d36
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-06 16:12:37 +0530

    Fix definition fetching for capitalized terms

--- app/components/definition/definitionStack.tsx
+++ app/components/definition/definitionStack.tsx
@@ -2,6 +2,7 @@ import DefinitionComponent from "@/app/components/definition/definition";
 import { Definition, Definitions } from "@/app/types/types";
 import { getDefinition } from "@/app/utils/api";
 import Stack from "@mui/material/Stack";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import DefinitionStackLoading from "./definitionStackLoading";
 import Error from "./error";
@@ -17,12 +18,27 @@ export default function DefinitionStack({
 }) {
   const [definitions, setDefinitions] = useState<Definitions>();
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchDefinition = async () => {
       try {
         setDefinitions(await getDefinition(term));
       } catch (error) {
+        // Check if the term is capitalized
+        // If so, try to fetch the definition with a lowercase term
+        if (term[0].toUpperCase() === term[0]) {
+          const newTerm = term[0].toLowerCase() + term.slice(1);
+          setError(
+            `Something went wrong, attempting to fetch definition as ${newTerm}`
+          );
+          router.replace(`/en/${newTerm}`);
+        }
+        // Add noindex to the page
+        const meta = document.createElement("meta");
+        meta.name = "robots";
+        meta.content = "noindex";
+        document.head.appendChild(meta);
         setError(`Failed to fetch definition as ${error}`);
       } finally {
         setLoading(false);
@@ -30,7 +46,7 @@ export default function DefinitionStack({
     };
 
     fetchDefinition();
-  }, [term, setLoading]);
+  }, [term, setLoading, router]);
 
   return (
     <>
