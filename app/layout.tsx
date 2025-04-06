0a33747bfaf89a4e11ece9654b39ed4d21954800
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-06 17:05:38 +0530

    Modify title

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Kwiktionary",
+  title: "Kwiktionary: Wiktionary, Reimagined",
   description:
     "Fast, clean, and open-source access to the vast knowledge of Wiktionary.",
 };
