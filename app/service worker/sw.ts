b40e714e9072c621fc08f89e46efd6a2e64f367c
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-07 21:44:30 +0530

    Fix service worker

--- app/service worker/sw.ts
+++ app/service worker/sw.ts
@@ -1,12 +1,16 @@
-import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import type {
+  PrecacheEntry,
+  RuntimeCaching,
+  SerwistGlobalConfig,
+} from "serwist";
 import {
   CacheableResponsePlugin,
   CacheFirst,
   ExpirationPlugin,
-  NavigationRoute,
   Serwist,
+  StaleWhileRevalidate,
 } from "serwist";
+import { API_URL } from "../utils/api";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -16,35 +20,67 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
+const cache: RuntimeCaching[] = [
+  {
+    matcher: /\.(?:js|css|json|woff2|png|ico|webmanifest)$/i,
+    handler: new CacheFirst({
+      cacheName: "static-resources",
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200] }),
+        new ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        }),
+      ],
+    }),
+  },
+  {
+    matcher: ({ url }) => {
+      return (
+        url.origin === new URL(API_URL).origin &&
+        url.pathname.includes("define")
+      );
+    },
+    handler: new StaleWhileRevalidate({
+      cacheName: "api-definition-cache",
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200, 404] }),
+        new ExpirationPlugin({
+          maxEntries: 100,
+          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+        }),
+      ],
+    }),
+  },
+  {
+    matcher: ({ url }) => {
+      return (
+        url.origin === new URL(API_URL).origin &&
+        url.pathname.includes("search")
+      );
+    },
+    handler: new CacheFirst({
+      cacheName: "api-search-cache",
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200] }),
+        new ExpirationPlugin({
+          maxEntries: 100,
+          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+        }),
+      ],
+    }),
+  },
+];
+
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: cache,
+  precacheOptions: {
+    cleanupOutdatedCaches: true,
+  },
 });
 
-// Cache strategy for /en/* route
-const enDefinitionStrategy = new CacheFirst({
-  cacheName: "kwiktionary-en-shell",
-  plugins: [
-    new CacheableResponsePlugin({ statuses: [200] }),
-    new ExpirationPlugin({
-      maxEntries: 1,
-      maxAgeSeconds: 60 * 60 * 24, // 1 day
-    }),
-    {
-      cacheKeyWillBeUsed: async ({}) => {
-        return "en/_app_shell";
-      },
-    },
-  ],
-});
-
-serwist.registerRoute(
-  new NavigationRoute(enDefinitionStrategy, {
-    allowlist: [/^\/en\/(?=.)/],
-  })
-);
-
-// TODO: cache API responses
+serwist.addEventListeners();
