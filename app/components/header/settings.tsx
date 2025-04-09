47af4313960d58c29f83ad49ddc05f9a53e66bcf
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-09 14:55:07 +0530

    Add PWA install prompt handling

--- app/components/header/settings.tsx
+++ app/components/header/settings.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Card,
   Collapse,
   FormControl,
@@ -8,10 +9,67 @@ import {
   Typography,
   useColorScheme,
 } from "@mui/material";
-import { MoonStar, Sun, SunMoon } from "lucide-react";
+import { Download, MoonStar, Sun, SunMoon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
 
 export default function Settings({ open }: { open: boolean }) {
   const { mode, setMode } = useColorScheme();
+  const [installSupport, setInstallSupport] = useState<boolean>(false);
+  const [installPrompt, setInstallPrompt] =
+    useState<BeforeInstallPromptEvent | null>(null);
+
+  const handleThemeToggle = () => {
+    setMode(
+      mode === "dark"
+        ? "light"
+        : mode === "light"
+        ? "system"
+        : mode === "system"
+        ? "dark"
+        : "dark"
+    );
+  };
+
+  const handleInstall = async () => {
+    if (!installPrompt) {
+      return;
+    }
+    await installPrompt.prompt();
+    const { outcome } = await installPrompt.userChoice;
+    if (outcome === "accepted") {
+      setInstallSupport(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event: Event) => {
+      event.preventDefault();
+      setInstallSupport(true);
+      setInstallPrompt(event as BeforeInstallPromptEvent); // Cast the event
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    window.addEventListener("appinstalled", () => {
+      setInstallSupport(false);
+      setInstallPrompt(null);
+    });
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
 
   return (
     <Collapse
@@ -34,46 +92,62 @@ export default function Settings({ open }: { open: boolean }) {
           Settings
         </Typography>
         <Box sx={{ px: 2, pb: 2 }}>
-          <FormControl
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <FormLabel id="theme-toggle">
-              <Typography>
-                <strong>Theme</strong>
-              </Typography>
-            </FormLabel>
-            <Box>
-              <Typography
-                component="span"
-                variant="body2"
-                sx={{
-                  color: "text.secondary",
-                }}
-              >{`(${mode})`}</Typography>
+          <FormControl sx={{ width: "100%" }}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <FormLabel id="theme-toggle">
+                <Typography>
+                  <strong>Theme</strong>
+                </Typography>
+              </FormLabel>
+              <Box>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  sx={{
+                    color: "text.secondary",
+                  }}
+                >{`(${mode})`}</Typography>
 
-              <IconButton
-                onClick={() =>
-                  setMode(
-                    mode === "dark"
-                      ? "light"
-                      : mode === "light"
-                      ? "system"
-                      : mode === "system"
-                      ? "dark"
-                      : "dark"
-                  )
-                }
-                aria-label="Toggle theme"
+                <IconButton
+                  onClick={handleThemeToggle}
+                  aria-label="Toggle theme"
+                >
+                  {mode === "dark" && <MoonStar />}
+                  {mode === "light" && <Sun />}
+                  {mode === "system" && <SunMoon />}
+                </IconButton>
+              </Box>
+            </Box>
+            <Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+                hidden={!installSupport}
               >
-                {mode === "dark" && <MoonStar />}
-                {mode === "light" && <Sun />}
-                {mode === "system" && <SunMoon />}
-              </IconButton>
+                <FormLabel id="download">
+                  <Typography>
+                    <strong>Install</strong>
+                  </Typography>
+                </FormLabel>
+
+                <Button
+                  variant="outlined"
+                  size="small"
+                  startIcon={<Download />}
+                  onClick={handleInstall}
+                >
+                  Install
+                </Button>
+              </Box>
             </Box>
           </FormControl>
         </Box>
