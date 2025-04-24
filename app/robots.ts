39a7a63aab7268bd36f0bb78f01120e6759e3878
Jake Anto <64896514+jake-anto@users.noreply.github.com>
2025-04-25 01:41:48 +0530

    Disallow AI crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,18 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      crawlDelay: 5,
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        crawlDelay: 30,
+      },
+      {
+        // Disallow AI and non-search engine bots
+        userAgent: ["meta-externalagent", "GoogleOther", "SemrushBot"],
+        disallow: "/",
+      },
+    ],
     sitemap: "https://kwiktionary.vercel.app/sitemap.xml",
   };
 }
